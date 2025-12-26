b0b216d7eccc4089a203dc5809a7f568cc8455d9
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-26 11:04:01 +0000

    Add CRUD operations

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const typeDefs = gql `
 
   type Mutation { 
   createUser(name: String!, email: String!, age: Int!): User!
+  updateUser(id:ID!, name: String, email: String): User!
+  deleteUser(id:ID!): User!
   }
    `
    ;
@@ -28,6 +30,30 @@ const resolvers = {
             const user = {id: users.length + 1 , name,email,age};
             users.push(user)
             return user;
+        },
+        updateUser:(_, { id, name, email}) =>{
+            const numericId = Number(id)
+            const user = users.find(u => u.id === numericId);
+            if(!user){
+                throw new Error("User not found")
+            }
+            if (name !== undefined){
+                user.name = name
+            }
+            if ( email !== undefined){
+                user.email = email
+            }
+            return user
+        },
+        deleteUser:(_,{id}) =>{
+            const numericId = Number(id)
+            const index = users.findIndex(u => u.id === numericId)
+            if (index === -1){
+                throw new Error("User not found");
+            }
+            const deletedUser = users[index];
+            users.splice(index,1);
+            return deletedUser
         }
     }
 };
